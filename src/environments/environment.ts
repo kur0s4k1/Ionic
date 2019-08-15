efa878f108f26f68823e6134f7d75781357b36d2
Aluno <joao12308@hotmail.com>
2019-08-15 20:32:03 -0300

    Alterando o caminho do Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyAzFpGVD3r2ppclg-PbDQWUDwa7Ci0Vw8g",
-    authDomain: "teste-1dba4.firebaseapp.com",
-    databaseURL: "https://teste-1dba4.firebaseio.com",
-    projectId: "teste-1dba4",
-    storageBucket: "",
-    messagingSenderId: "479368098060",
-    appId: "1:479368098060:web:38bad08d9c4a6c89"
+    apiKey: "AIzaSyDt01Z2DMjishQqJBIKC1QoYXPMPxw-CRM",
+    authDomain: "gfamr-241719.firebaseapp.com",
+    databaseURL: "https://gfamr-241719.firebaseio.com",
+    projectId: "gfamr-241719",
+    storageBucket: "gfamr-241719.appspot.com",
+    messagingSenderId: "389509075655",
+    appId: "1:389509075655:web:27b6fd71786f0960"
   }
-
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
